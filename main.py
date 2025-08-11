b25c493ce683751d3ea6ef9f4d3ebcc3df97e9f8
Samuel Azubuike <chideracent0@gmail.com>
2025-08-11 13:46:52 +0100

    added the command-line argument support

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import argparse
 import requests
 
-username = input("Enter Github Username:")
+
+#setting up argurment parser
+parser = argparse.ArgumentParser(description="Fetch recent activity for user")
+parser.add_argument("username", help="Github username to fetch activity for")
+parser.add_argument("-n", "--number", type=int, default=5, help="Number of events to display")
+
+#parse the argurment
+args = parser.parse_args()
 
 #calling Github api
 Url = f"https://api.github.com/users/{username}/events"
@@ -13,8 +21,10 @@ response = requests.get(Url)
 #checking response status and handling errors
 if response.status_code == 200:
     events = response.json()
-
-    for event in events[:10]:
-        print(f"- {event['type']} at {event['created_at']}")
+    if not events:
+        print(f"No recent activity found for user '{args.username}'.")
     else:
+        for event in events[:args.number]:
+             print(f"- {event['type']} at {event['created_at']}")
+else:
         print(f"error: {response.status_code} could cot fetch data")
